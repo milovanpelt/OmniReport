86ea7a5914235383629f19fc2b6fa3a117cbb579
Milo van Pelt <milovanpelt@gmail.com>
2026-04-12 23:57:11 +0200

    feat: message can be send to discord hook bot, using cpr

--- Source/OmniReport.cpp
+++ Source/OmniReport.cpp
@@ -1,8 +1,18 @@
 #include "pch.h"
 #include "OmniReport.h"
 #include <iostream>
+#include <cpr/cpr.h>
 
-OMNIREPORT_API void PrintTestMessage(char* message)
+OMNIREPORT_API void PrintTestMessage(char* url, char* message)
 {
 	std::cout << message << "\n";
+	
+	std::string json_payload = "{\"content\": \"" + std::string(message) + "\"}";
+
+	cpr::Response r = cpr::Post(cpr::Url{ url },
+		cpr::Body{ json_payload },
+		cpr::Header{ {"Content-Type", "application/json"} });
+	std::cout << r.text << std::endl;
+
+	std::cout << json_payload << "\n";
 }
