35f1590dbb0ce3484026887f913f5dffc9f6f5d3
Milo van Pelt <milovanpelt@gmail.com>
2026-04-13 03:11:10 +0200

    feat(omnireport): test bot setup using DiscordClient

--- Source/OmniReport.cpp
+++ Source/OmniReport.cpp
@@ -2,17 +2,10 @@
 #include "OmniReport.h"
 #include <iostream>
 #include <cpr/cpr.h>
+#include "DiscordClient.h"
 
-OMNIREPORT_API void PrintTestMessage(char* url, char* message)
+OMNIREPORT_API void PrintTestMessage(char* message)
 {
-	std::cout << message << "\n";
-	
-	std::string json_payload = "{\"content\": \"" + std::string(message) + "\"}";
-
-	cpr::Response r = cpr::Post(cpr::Url{ url },
-		cpr::Body{ json_payload },
-		cpr::Header{ {"Content-Type", "application/json"} });
-	std::cout << r.text << std::endl;
-
-	std::cout << json_payload << "\n";
+	DiscordClient testBot("");
+	testBot.SendMessage(message);
 }
